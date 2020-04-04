40bde0f7b79bbb9430842a09a18e526bbb974be1
Avishrant Sharma <avishrants@gmail.com>
2020-04-04 14:56:43 +0530

    Added Road Radius Identification

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
-from lib_rad import radius
-from easygui import fileopenbox
 import cv2
+import numpy as np
 
-#img_path = fileopenbox()
-origimg = cv2.imread("/home/avishrant/GitRepo/automob/TestData/circle.png")
-(thresh, img) = cv2.threshold(cv2.cvtColor(origimg, cv2.COLOR_BGR2GRAY), 127, 255, cv2.THRESH_BINARY)
+img = cv2.imread("/home/avishrant/GitRepo/automob/TestData/road1.jpeg")
+cimg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+circles = cv2.HoughCircles(cimg,cv2.HOUGH_GRADIENT,1,100,param1=50,param2=30,minRadius=10 ,maxRadius=200)
 
-radius = radius(img)
-radius.findrad()
+if circles is not None:
+    circles = np.uint16(np.around(circles))
+    for i in circles[0,:]:
+        # draw the outer circle
+        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+        # draw the center of the circle
+        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
-cv2.imshow("Image", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        cv2.imshow('detected circles',cimg)
+        cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
